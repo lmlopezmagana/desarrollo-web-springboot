4809020c9f45575d2d16a8f3b05308dd28bd3165
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-26 23:36:11 +0200

    3.1 Controladores

---
+++ web/src/main/java/com/openwebinars/web/MainController.java
@@ -0,0 +1,45 @@
+package com.openwebinars.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("message", "Hola Mundo!");
+        return "index";
+    }
+
+    @GetMapping("/index")
+    public ModelAndView welcomeV2() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("message", "Hola Mundo!");
+        return modelAndView;
+    }
+
+    @ModelAttribute("otroSaludo")
+    public String otroSaludo() {
+        return "Espero aprender mucho con este curso de Desarrollo web con Spring Boot";
+    }
+
+    @GetMapping("/other1")
+    @ResponseBody
+    public String responseString() {
+        return "Hola Mundo!!!";
+    }
+
+
+    @GetMapping("/other2")
+    public ResponseEntity<String> responseEntity() {
+        return ResponseEntity.ok("Hola Mundo!!!");
+    }
+
+
+}
