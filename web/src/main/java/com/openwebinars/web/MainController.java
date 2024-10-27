7c9f20fda63f6b56650785b12bd9a21ff351c455
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-27 09:48:08 +0100

    3.3 Manejo de parámetros

--- web/src/main/java/com/openwebinars/web/MainController.java
+++ web/src/main/java/com/openwebinars/web/MainController.java
@@ -3,10 +3,12 @@ package com.openwebinars.web;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.awt.geom.Line2D;
+import java.util.Optional;
 
 @Controller
 public class MainController {
@@ -50,6 +52,34 @@ public class MainController {
     public String forward() {
         return "forward:/";
     }
+
+    @GetMapping("/producto/{id:[0-9]+}")
+    public String ejemploPathVariable(
+            @PathVariable("id") Long id,
+            Model model) {
+        model.addAttribute("id", id);
+        return "producto";
+    }
+
+    @GetMapping("/saludo")
+    public String ejemploQueryParam(
+            @RequestParam(
+                    name = "nombre",
+                    required = false,
+                    defaultValue = "User") String nombre,
+            Model model) {
+        String msg = "Hola, " + nombre;
+        model.addAttribute("message", msg);
+        return "index";
+    }
+
+
+    @GetMapping("/multimap")
+    public ResponseEntity<String> ejemploMultiValueMap(
+            @RequestParam MultiValueMap<String, String> params) {
+        return ResponseEntity.ok(params.toString());
+    }
+
 
 
 
