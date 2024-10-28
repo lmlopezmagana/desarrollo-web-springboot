ee3d3d03468f2d5c7518c07d6aa8af9e8f3fbc41
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2024-10-28 23:52:38 +0100

    7.4 @AuthenticationPrincipal
    Minor bugs fixed

--- web-data/src/main/java/com/openwebinars/webdata/utils/GlobalExceptionHandler.java
+++ web-data/src/main/java/com/openwebinars/webdata/utils/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.openwebinars.webdata.utils;
 
 import com.openwebinars.webdata.exception.ProductoNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
-import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
